e306c27c5ffbaf399dd3b64a34e3348a85d5d84e
Chris Clynes <chrisclynes@live.com>
2022-04-08 23:26:39 -0400

    added search features and filters

--- pages/index.js
+++ pages/index.js
@@ -5,19 +5,19 @@ import { Flex, Box, Text, Button } from '@chakra-ui/react';
 import Property from '../components/Property'
 import { baseUrl, fetchApi } from '../utils/fetchApi';
 
-const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl }) => (
-  <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
-    <Image src={imageUrl} width={500} height={300} alt="banner" /> 
-    <Box p="5">
-      <Text color="gray.500" fontSize="sm" fontWeight="medium">{purpose}</Text>
-      <Text fontSize="3x1" fontWeight="bold">{title1}<br />{title2}</Text>
-      <Text fontSize="1g" fontWeight="medium" paddingTop="3" paddingBottom="3" color="gray.700">{desc1}</Text>
-      <Button fontSize="x1">
-        <Link href={linkName}>{buttonText}</Link>
+export const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl }) => (
+  <Flex flexWrap='wrap' justifyContent='center' alignItems='center' m='10'>
+    <Image src={imageUrl} width={500} height={300} />
+    <Box p='5'>
+      <Text color='gray.500' fontSize='sm' fontWeight='medium'>{purpose}</Text>
+      <Text fontSize='3xl' fontWeight='bold'>{title1}<br />{title2}</Text>
+      <Text fontSize='lg' paddingTop='3' paddingBottom='3' color='gray.700'>{desc1}<br />{desc2}</Text>
+      <Button fontSize='xl' bg="blue.300" color="white">
+        <Link href={linkName}><a>{buttonText}</a></Link>
       </Button>
     </Box>
   </Flex>
-)
+);
 
 export default function Home({ propertiesForSale, propertiesForRent }) {
   console.log(propertiesForSale, propertiesForRent)
@@ -27,7 +27,7 @@ export default function Home({ propertiesForSale, propertiesForRent }) {
         purpose="RENT A HOME"
         title1="Rental Homes for"
         title2="Everyone"
-        desc="Explore Apartments, Villas, Homes"
+        desc1="Explore Apartments, Villas, Homes"
         desc2="and more"
         buttonText="Explore Renting"
         linkName="/search?purpose=for-rent"
@@ -42,7 +42,7 @@ export default function Home({ propertiesForSale, propertiesForRent }) {
         purpose="BUY A HOME"
         title1="Find, Buy and Own Your"
         title2="Dream Home"
-        desc="Explore Apartments, Villas, Homes"
+        desc1="Explore Apartments, Villas, Homes"
         desc2="and more"
         buttonText="Explore Buying"
         linkName="/search?purpose=for-sale"
